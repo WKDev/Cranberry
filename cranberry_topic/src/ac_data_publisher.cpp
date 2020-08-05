3a5600597cb2bba3b6a48a5b41e403279cd8cbc3
WKDev <chanhyukman@gmail.com>
2020-08-05 11:11:35 -0700

    roslibjs can parse data from client from now on.

--- cranberry_topic/src/ac_data_publisher.cpp
+++ cranberry_topic/src/ac_data_publisher.cpp
@@ -11,17 +11,18 @@ ros::NodeHandle nh;
 // 퍼블리셔 큐(queue) 사이즈를 100개로 설정한다는 것이다
 ros::Publisher ac_temp_pub = nh.advertise<cranberry_topic::AcData>("ac_msg", 100);
 // 루프 주기를 설정한다. "10" 이라는 것은 10Hz를 말하는 것으로 0.1초 간격으로 반복된다
-ros::Rate loop_rate(5);
+ros::Rate loop_rate(2);
 // MsgTutorial 메시지 파일 형식으로 msg 라는 메시지를 선언
 cranberry_topic::AcData ad;
 // 메시지에 사용될 변수 선언
+ROS_INFO("ac_data_publisher is publishing ac_data...\n Ctrl+C to terminate."); // stamp.sec 메시지를 표시한다
 
 while (ros::ok())
 {
 ad.temp = 25.0 + rand()%3*0.8; // 현재 시간을 msg의 하위 stamp 메시지에 담는다
 ad.humid = 50.0 + rand()%15*0.8; // count라는 변수 값을 msg의 하위 data 메시지에 담는다
-ROS_INFO("send temp = %f", ad.temp); // stamp.sec 메시지를 표시한다
-ROS_INFO("send humid = %f", ad.humid); // stamp.nsec 메시지를 표시한다
+// ROS_INFO("send temp = %f", ad.temp); // stamp.sec 메시지를 표시한다
+// ROS_INFO("send humid = %f", ad.humid); // stamp.nsec 메시지를 표시한다
 ac_temp_pub.publish(ad); // 메시지를 발행한다
 loop_rate.sleep(); // 위에서 정한 루프 주기에 따라 슬립에 들어간다
 }
